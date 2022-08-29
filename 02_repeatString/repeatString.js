2a6f221aa93537544a5279256109766d061921df
Danny Neel <dneel10@gmail.com>
2022-08-29 00:22:43 -0400

    complete exercise 01 and 02 with all tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
 
-};
+const repeatString = function(string, num) {
+    let newString='';
+    if (num < 0) {
+        newString += "ERROR";
+    } else {
+        for (let i = 0; i < num; i++){
+            newString += string;
+        }
+    }
+    return newString;
+}
+let outputString = '';
+repeatString('hey', 3);
+
 
 // Do not edit below this line
 module.exports = repeatString;
