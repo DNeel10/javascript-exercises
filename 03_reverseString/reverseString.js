ae8a364fabf225e295e2f00cc9badddcb7bf6661
Danny Neel <dneel10@gmail.com>
2022-08-29 07:47:36 -0400

    complete exercise 03 and passed all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string){
+    let reversedString = '';
+    const splitString = string.split("");
+    while (splitString.length > 0){
+        reversedString += splitString[splitString.length-1];
+        splitString.pop();
+    }
+    return reversedString;
+}
 
-};
 
 // Do not edit below this line
 module.exports = reverseString;
