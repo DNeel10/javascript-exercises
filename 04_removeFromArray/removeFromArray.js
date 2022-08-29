627fb57ed1e44af93f4434f053dab714aba0846c
Danny Neel <dneel10@gmail.com>
2022-08-29 10:49:46 -0400

    complete exercise 4 with all tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,24 @@
-const removeFromArray = function() {
+// set up function to remove "removal" from an array "array". Removal can be any number of items.
+// if "removal" is multiple arguments, cycle through each one
+// remove each item from the original array.
 
+
+const removeFromArray = function(array, ...removal) {
+    for (const item of removal){
+        let index = array.indexOf(item);
+        if (index !== -1){
+            array.splice(index,1);
+        }
+    }
+    return array;
 };
-
+/*const removeFromArray = function(array, removal) {
+    let index = array.indexOf(removal);
+    if (index !== -1){
+        array.splice(index,1);
+        return array;
+    } 
+};
+*/
 // Do not edit below this line
 module.exports = removeFromArray;
