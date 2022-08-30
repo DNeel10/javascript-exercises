43cb9f4bada525004e1e44704616646da014494f
Danny Neel <dneel10@gmail.com>
2022-08-29 23:32:24 -0400

    update exercise 06 with 6/6 tests passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,19 @@
-const leapYears = function() {
+// set up function to accept a year as an argument
+//      const leapYears = function(year) {}
+// test conditions
+// return true/false
+
+
+const leapYears = function(year) {
+    if ((year % 400) === 0 && (year % 100) === 0){
+        return true;
+    } else if ((year % 100 === 0) && (year % 400) !== 0){
+        return false;
+    } else if ((year % 4) === 0){
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
