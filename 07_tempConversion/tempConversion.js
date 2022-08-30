27c92eeb3aa31780e93e542096d590191579e255
Danny Neel <dneel10@gmail.com>
2022-08-30 09:14:16 -0400

    update exercise 08 with 16/16 tests passed. Use Recursion for factorial calculation. clean up exercise 07 syntax to make code more readable

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -6,22 +6,12 @@ const ftoc = function(fDeg) {
   let celcius = (fDeg - 32) * (5/9);
   const cRounded = Number(celcius.toFixed(1));
   return cRounded;
-  /*if (cRounded === 0.0){
-    return 0;
-  } else {
-    return cRounded;
-  }*/
 };
 
 const ctof = function(cDeg) {
   let farenheight = (cDeg *(9/5)) + 32
   const fRounded = Number(farenheight.toFixed(1));
   return fRounded;
-  /*if (fRounded === 32.0){  
-    return 32;
-  } else {
-    return fRounded;
-  }*/
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,41 @@
-const add = function() {
-	
+const add = function(valueOne,valueTwo) {
+  let addTotal = valueOne + valueTwo;
+  return addTotal;
 };
 
-const subtract = function() {
-	
+const subtract = function(subOne, subTwo) {
+	let subtractDiff = subOne - subTwo;
+  return subtractDiff;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let sumTotal = 0;
+  for (const number of numbers){
+    sumTotal += number;
+  }
+  return sumTotal;
 };
 
-const multiply = function() {
-
+const multiply = function(multiples) {
+  let multTotal = 1;
+  for (const multiple of multiples){
+    multTotal *= multiple;
+  }
+  return multTotal;
 };
 
-const power = function() {
-	
+const power = function(numOne, exp) {
+	let powerTotal = numOne**exp;
+  return powerTotal;
 };
 
-const factorial = function() {
-	
-};
+const factorial = function(factValue) {
+  if ((factValue === 1) || (factValue === 0)){
+    return 1;
+  } else {
+    return factValue * factorial(factValue-1);
+  } 
+}
 
 // Do not edit below this line
 module.exports = {
