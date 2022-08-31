c47e7ca7423a05bb17d93307667b90583034db17
Danny Neel <dneel10@gmail.com>
2022-08-31 00:31:48 -0400

    update exercise 11 with 1/1 tests passed.  show original method using for... of loop.  updated to run with map method.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,19 @@
-const getTheTitles = function() {
+// return array of titles
+// 
+// const books = [{title: 'Book', author: 'Name'},{title: 'Book2', author: 'Name2'}]
+    
 
+
+
+const getTheTitles = function(array) {
+ /*   let titles = []
+    for (const book of array){
+        const booktitles = Object.getOwnPropertyDescriptors(book);
+        titles.push(booktitles.title.value);
+    }
+    return titles;*/
+    const map1 = array.map(books => books.title);
+    return map1;
 };
-
 // Do not edit below this line
 module.exports = getTheTitles;
